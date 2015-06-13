55549620fad7b791f9001486f2cb72dae9184c4e
keen <m.d.trey@gmail.com>
2015-06-13 01:21:51 -0400

    Repo Creation

---
+++ art/shapes/trey/materials.cs
@@ -0,0 +1,125 @@
+
+singleton Material(logcabin_trey_logcabin_material)
+{
+   mapTo = "trey_logcabin_material";
+   translucentBlendOp = "None";
+};
+
+singleton Material(logcabin_trey_logcabin_Color_F13_)
+{
+   mapTo = "trey_logcabin_Color_F13_";
+   diffuseColor[0] = "0.419608 0.6 0.239216 0.960784";
+   translucent = "1";
+};
+
+singleton Material(logcabin_trey_logcabin_material_1)
+{
+   mapTo = "trey_logcabin_material_1";
+   translucentBlendOp = "None";
+};
+
+singleton Material(logcabin_trey_logcabin_material_5)
+{
+   mapTo = "trey_logcabin_material_5";
+   diffuseMap[0] = "art/shapes/trey/logcabin/material_5";
+   translucentBlendOp = "None";
+};
+
+singleton Material(logcabin_trey_logcabin_Color_001_)
+{
+   mapTo = "trey_logcabin_Color_001_";
+   diffuseColor[0] = "0.886275 0.886275 0.886275 1";
+   translucentBlendOp = "None";
+};
+
+singleton Material(logcabin_trey_logcabin_material_6)
+{
+   mapTo = "trey_logcabin_material_6";
+   diffuseMap[0] = "art/shapes/trey/logcabin/material_6";
+   translucentBlendOp = "None";
+};
+
+singleton Material(logcabin_trey_logcabin_material_8)
+{
+   mapTo = "trey_logcabin_material_8";
+   diffuseMap[0] = "art/shapes/trey/logcabin/material_8";
+   translucentBlendOp = "None";
+};
+
+singleton Material(logcabin_trey_logcabin_Wood__Floor_)
+{
+   mapTo = "trey_logcabin_Wood__Floor_";
+   diffuseColor[2] = "White";
+   diffuseMap[0] = "art/shapes/trey/logcabin/Wood__Floor_";
+   translucentBlendOp = "None";
+};
+
+singleton Material(logcabin_trey_logcabin_Roofing_Shingles_Asphalt_)
+{
+   mapTo = "trey_logcabin_Roofing_Shingles_Asphalt_";
+   diffuseMap[0] = "art/shapes/trey/logcabin/Roofing_Shingles_Asphalt_";
+   translucentBlendOp = "None";
+};
+
+singleton Material(tos_familiars_Material_001)
+{
+   mapTo = "Material_001";
+   diffuseColor[0] = "0.0180837 0.00957294 0.64 1";
+   specular[0] = "0.5 0.5 0.5 1";
+   specularPower[0] = "50";
+   translucentBlendOp = "None";
+};
+
+singleton Material(tos_bldg02_Material_002)
+{
+   mapTo = "Material_002";
+   diffuseColor[0] = "0.64 0.0406231 0.0664598 1";
+   specular[0] = "0.5 0.5 0.5 1";
+   specularPower[0] = "50";
+   translucentBlendOp = "None";
+};
+
+singleton Material(tos_bldg03_Material_003)
+{
+   mapTo = "Material_003";
+   diffuseColor[0] = "0.64 0.64 0.64 1";
+   specular[0] = "0.5 0.5 0.5 1";
+   specularPower[0] = "50";
+   translucentBlendOp = "None";
+};
+
+singleton Material(tos_bldg04_Material_004)
+{
+   mapTo = "Material_004";
+   diffuseColor[0] = "0.64 0.64 0.64 1";
+   specular[0] = "0.5 0.5 0.5 1";
+   specularPower[0] = "50";
+   translucentBlendOp = "None";
+};
+
+singleton Material(tos_bldg05_Material_005)
+{
+   mapTo = "Material_005";
+   diffuseColor[0] = "0.64 0.64 0.64 1";
+   specular[0] = "0.5 0.5 0.5 1";
+   specularPower[0] = "50";
+   translucentBlendOp = "None";
+};
+
+singleton Material(tos_citywall_Material_006)
+{
+   mapTo = "Material_006";
+   diffuseColor[0] = "0.64 0.64 0.64 1";
+   specular[0] = "0.5 0.5 0.5 1";
+   specularPower[0] = "50";
+   translucentBlendOp = "None";
+};
+
+singleton Material(tos_bldg07_Material)
+{
+   mapTo = "Material";
+   diffuseColor[0] = "0.64 0.64 0.64 1";
+   specular[0] = "0.5 0.5 0.5 1";
+   specularPower[0] = "50";
+   translucentBlendOp = "None";
+};
